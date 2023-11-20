70d493c0dd386026a46feba0f61babaa41eb6cfb
Ram Rachum <ram@rachum.com>
2023-11-20 19:47:45 +0200

    -

--- pax/experiment.py
+++ pax/experiment.py
@@ -117,13 +117,13 @@ def global_setup(args):
         if args.debug:
             args.wandb.group = "debug-" + args.wandb.group
         run = wandb.init(
-            reinit=True,
-            entity=str(args.wandb.entity),
+            # reinit=True,
+            # entity=str(args.wandb.entity),
             project=str(args.wandb.project),
-            group=str(args.wandb.group),
-            name=str(args.wandb.name),
-            mode=str(args.wandb.mode),
-            tags=args.wandb.tags,
+            # group=str(args.wandb.group),
+            # name=str(args.wandb.name),
+            # mode=str(args.wandb.mode),
+            # tags=args.wandb.tags,
             config=omegaconf.OmegaConf.to_container(
                 args, resolve=True, throw_on_missing=True
             ),  # type: ignore
@@ -511,7 +511,7 @@ def agent_setup(args, env, env_params, logger):
             num_iterations=num_iterations,
             player_id=player_id,
         )
-    
+
 
     def get_LOLA_agent(seed, player_id):
         return make_lola(
